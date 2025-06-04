e995cde0110effff4644eec3843bd11991a94022
JoshDoro <josh.j.doro@gmail.com>
2025-06-03 21:13:39 -0500

    Started the basic migration to hugo from jekyll using the github copilot agent preview

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.md',
+    './archetypes/**/*.md',
+    './assets/**/*.js',
+    './assets/**/*.css',
+    './themes/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
